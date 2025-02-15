33fbe066e0ce2223007c831a79ac71186e3b3f9b
azeemhdr <comazeem@gmail.com>
2025-02-16 01:09:56 +0530

    responsive complete

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,14 +2,23 @@ $(document).ready(function(){
 		$(".partner-carousel").slick({
 		  slidesToShow: 8,
 		  slidesToScroll: 1,
-		//   dots: true,
-		  infinite: true,
+		  infinite: false,
 		  speed: 500,
-		//   fade: true,
 		  cssEase: "linear",
 		  autoplay: true,
 		  autoplaySpeed: 2000,
 		  arrows: false,
+		  responsive:[
+			{
+			breakpoint: 576,
+			settings:{
+				slidesToShow:3,
+				slidesToScroll:1,
+				infinite:true,
+			}
+
+		  }
+		  ]
 		//   prevArrow: '<button type="button" class="slick-prev">←</button>',
 		//   nextArrow: '<button type="button" class="slick-next">→</button>',
 		});
