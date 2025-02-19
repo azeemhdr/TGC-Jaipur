d13a05016d4cb4dbb018bf1d33ee0d54057a5ba4
azeemhdr <comazeem@gmail.com>
2025-02-19 15:26:35 +0530

    contact-us from done

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,39 +1,38 @@
 $(document).ready(function(){
-		$(".partner-carousel").slick({
-		  slidesToShow: 8,
-		  slidesToScroll: 1,
-		  infinite: true,
-		  speed: 500,
-		  cssEase: "linear",
-		  autoplay: true,
-		  autoplaySpeed: 2000,
-		  arrows: false,
-		  responsive:[
-			{
-			breakpoint: 576,
-			settings:{
-				slidesToShow:3,
-				slidesToScroll:1,
-				infinite:true,
-			}
+		// $(".partner-carousel").slick({
+		//   slidesToShow: 8,
+		//   slidesToScroll: 1,
+		//   infinite: true,
+		//   speed: 500,
+		//   cssEase: "linear",
+		//   autoplay: true,
+		//   autoplaySpeed: 2000,
+		//   arrows: false,
+		//   responsive:[
+		// 	{
+		// 	breakpoint: 576,
+		// 	settings:{
+		// 		slidesToShow:3,
+		// 		slidesToScroll:1,
+		// 		infinite:true,
+		// 	}
 
-		  }
-		  ]
-		//   prevArrow: '<button type="button" class="slick-prev">←</button>',
-		//   nextArrow: '<button type="button" class="slick-next">→</button>',
-		});
+		//   }
+		//   ]
+		
+		// });
 
-		$(".a-testimonial").slick({
-			slidesToShow:1,
-			slidesToScroll:1,
-			dots:true,
-			infinite:true,
-			autoplay:true,
-			autoplaySpeed:2000,
-			arrows:false,
-			fade:true,
-			cssEase:"linear"
-		});
+		// $(".a-testimonial").slick({
+		// 	slidesToShow:1,
+		// 	slidesToScroll:1,
+		// 	dots:true,
+		// 	infinite:true,
+		// 	autoplay:true,
+		// 	autoplaySpeed:2000,
+		// 	arrows:false,
+		// 	fade:true,
+		// 	cssEase:"linear"
+		// });
 
 		$(".question").click(function () {
 			$(this).next(".answerPanel").slideToggle("slow");
