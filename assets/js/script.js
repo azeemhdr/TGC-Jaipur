12c97270964b0c8ee0817033ccc4e6b309d20db1
azeemhdr <comazeem@gmail.com>
2025-02-21 16:43:56 +0530

    module updated

--- assets/js/script.js
+++ assets/js/script.js
@@ -13,4 +13,19 @@ $(document).ready(function(){
 		e.preventDefault();
 		$('.modal_section').fadeOut();
 	  })
+	//Modules=====================================================
+	$(".module-learn-more").click(function () {
+		let hiddenContent = $(this).siblings(".hidden-content");
+		let shortText = $(this).siblings(".short-text");
+
+		if (hiddenContent.is(":visible")) {
+			hiddenContent.hide();
+			shortText.show();
+			$(this).text("Learn More");
+		} else {
+			hiddenContent.show();
+			shortText.hide();
+			$(this).text("Show Less");
+		}
+	});
 })
